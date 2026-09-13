5c49b81be30df087f8f85632765880ea69b7f9df
Srijan-Petwal <srijan8a@gmail.com>
2026-09-14 01:07:04 +0530

    structuring output

--- src/react_search_agent_traditional/main.py
+++ src/react_search_agent_traditional/main.py
@@ -1,74 +1,55 @@
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from langsmith import Client
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
-from langchain_core.prompts import PromptTemplate
+from langsmith import Client
 
-tools=[TavilySearch()]
-llm=ChatGoogleGenerativeAI(model='gemini-3.6-flash')
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.runnables import RunnableLambda
+
+from react_search_agent_traditional.prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTION
+from react_search_agent_traditional.schemas import AgentResponse
+
+tools = [TavilySearch()]
+llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
 client = Client()
-#react_prompt = client.pull_prompt("hwchase17/react",dangerously_pull_public_prompt=True)
+# react_prompt = client.pull_prompt("hwchase17/react",dangerously_pull_public_prompt=True)
 
-react_prompt = PromptTemplate.from_template("""
-Answer the following question as best you can. You have access to the following tools:
+#react_prompt = 
+output_parser=PydanticOutputParser(pydantic_object=AgentResponse)
+# defining react agent
 
-{tools}
 
-You MUST follow this exact format.
+react_prompt_with_format_instruction=PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTION,
+    input_variables={"input","agent_scratchpad","tool_names"}
+).partial(format_instructions=output_parser.get_format_instructions())
 
-Question: the input question you must answer
-Thought: your reasoning about what to do
-Action: one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-
-Repeat Thought/Action/Action Input/Observation as necessary.
-
-When you have enough information, you MUST finish with:
-
-Thought: I now know the final answer
-Final Answer: your answer to the original question
-
-IMPORTANT:
-- Do not write the final answer before "Final Answer:"
-- Do not use Markdown headings instead of the required format.
-- Do not output a normal answer until you reach "Final Answer:".
-- When you are finished, the last section MUST begin with "Final Answer:".
-
-Begin!
-
-Question: {input}
-Thought:{agent_scratchpad}
-""")
-
-#defining react agent
-agent=create_react_agent(
+agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt,
+    prompt=react_prompt_with_format_instruction,
 )
-
-#orchestrator
-agent_executor=AgentExecutor(agent=agent,tools=tools, verbose=True)
-chain=agent_executor
+# orchestrator
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
 
 
 def main() -> None:
     print("Hello from react-search-agent-traditional!")
-    
 
-    result=chain.invoke(
+    result = chain.invoke(
         input={
-            "input":"search for top 3 currently airing anime using langchain on myanimelist and list their details",
-            }
-        )
+            "input": "search for top anime using langchain on myanimelist and list its details",
+        }
+    )
     print(result)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
